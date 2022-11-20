2eac168d3400ce7d0ba3f9364f38c7ea837e5768
panta147 <dreamcometrue321@gmail.com>
2022-11-20 09:11:19 +0545

    ok change

--- account/api/views.py
+++ account/api/views.py
@@ -4,14 +4,7 @@ from rest_framework.response import Response
 
 
 class RegisterAPIView(APIView):
-<<<<<<< HEAD
-    def post(self,request):
-        data=request.data
-        # print(request)
-        return Response({"message":'success','status':'ok','data':data})
-=======
     def post(self, request):
         data = request.data
         # print(request)
-        return Response({"message": data, 'status': 'ok', 'admin': "amritpanta"})
->>>>>>> 1249fc6cb2f6ee027ea2df683d809ab783d8a8a2
+        return Response({"message": 'success', 'status': 'ok hai', 'data': data})
