ab1b78cd7ae6d9bce4ac90c95ef66b5deaecfcbb
panta147 <dreamcometrue321@gmail.com>
2022-11-20 08:21:49 +0545

    changes

--- account/api/views.py
+++ account/api/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
+
 class RegisterAPIView(APIView):
-    def post(self,request):
-        data=request.data
-        print(request)
-        return Response({"message":data,'status':'ok'})
+    def post(self, request):
+        data = request.data
+        # print(request)
+        return Response({"message": data, 'status': 'ok', 'admin': "amrit"})
